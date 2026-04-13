455d51a8efdaf76346d5384f3bb96b38fc86211a
fastigo-ai <fastigo.in@gmail.com>
2026-04-13 10:59:25 +0530

    run

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <VendorProvider>
+        
           <BrowserRouter>
             <AuthLoader>
               <Toaster />
